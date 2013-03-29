e1565a6f1a585002718f4b945115aa4b55864677
Ben Elsen <ben.elsen@gmail.com>
2013-03-29 19:10:06 +0100

    more comments

--- src/time/conversions.js
+++ src/time/conversions.js
@@ -4,28 +4,35 @@ import "../constants/time";
 /**
  * Common conversions between time standards
  * All time values are in milliseconds
+ * unless specified
  */
 
+// JD -> MJD [days]
 orb.time.JDtoMJD = function(jd) {
   return jd + orb.constants.time.MJDJD;
 }
 
+// MJD -> JD [days]
 orb.time.MJDtoJD = function(mjd) {
   return mjd - orb.constants.time.MJDJD;
 }
 
+// TAI -> TT
 orb.time.TAItoTT = function(tai) {
   return tai + orb.constants.time.TTTAI;
 }
 
+// TT -> TAI
 orb.time.TTtoTAI = function(tt) {
   return tt - orb.constants.time.TTTAI;
 }
 
+// TAI -> UTC
 orb.time.TAItoUTC = function(tai) {
   return tai - orb.constants.time.TAIUTC;
 }
 
+// UTC -> TAI
 orb.time.UTCtoTAI = function(utc) {
   return utc + orb.constants.time.TAIUTC;
 }
