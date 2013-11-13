8b42d326867ba3645edb9cf8aa36877061586446
Ben Elsen <ben.elsen@gmail.com>
2013-11-13 14:23:11 +0100

    Correct DUT1

--- src/constants/time.js
+++ src/constants/time.js
@@ -15,8 +15,9 @@ orb.constants.time = {
   // TT = TAI + 32.184 seconds
   TTTAI  : +32184,
 
-  // DUT1 = UT1 - UTC = +0.2
-  DUT1   : +200,
+  // DUT1 = UT1 - UTC = -0.1
+  // -0.1 seconds beginning 21 Nov 2013 at 0000 UTC
+  DUT1   : -100,
 
   // TAI - UTC = 35.000 seconds (Leap seconds) (valid until at least 2013-12-31)
   TAIUTC : +35000
