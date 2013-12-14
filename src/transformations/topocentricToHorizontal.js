f2805f36788638be5d66436cd81b7cc3a14c50e5
Ben Elsen <ben.elsen@gmail.com>
2013-12-14 21:56:15 +0100

    [ADD] horizontalToTopocentric

--- src/transformations/topocentricToHorizontal.js
+++ src/transformations/topocentricToHorizontal.js
@@ -1,5 +1,9 @@
 import "./";
 
+/**
+ * @param x = [x, y, z]
+ * @return [azimuth, elevation, distance]
+ */
 orb.transformations.topocentricToHorizontal = function(x) {
 
   var azimuth = ( 2*Math.PI + Math.atan2( x[1], x[0] ) ) % (2*Math.PI);
@@ -8,6 +12,20 @@ orb.transformations.topocentricToHorizontal = function(x) {
     azimuth, // Azimuth
     Math.atan2( x[2], Math.sqrt( Math.pow(x[0], 2) + Math.pow(x[1], 2)) ), // Elevation
     Math.sqrt( Math.pow(x[0], 2) + Math.pow(x[1], 2) + Math.pow(x[2], 2) ) // Distance
+  ];
+
+};
+
+/**
+ * @param x = [azimuth, elevation, distance]
+ * @return [x, y, z]
+ */
+orb.transformations.horizontalToTopocentric = function(x) {
+
+  return [
+    x[2] * Math.cos( x[1] ) * Math.cos( x[0] ), // x
+    x[2] * Math.cos( x[1] ) * Math.sin( x[0] ), // y
+    x[2] * Math.sin( x[1] )                     // z
   ];
 
 };
