3cfccc58e0f312b345d566666c63a024d1047666
Ben Elsen <ben.elsen@gmail.com>
2013-12-14 22:19:58 +0100

    [ADD] fixedToInertial

--- src/transformations/inertialToFixed.js
+++ src/transformations/inertialToFixed.js
@@ -8,5 +8,9 @@ orb.transformations.inertialToFixed = function(x, Δt, ω, axis) {
   if ( !ω ) ω = orb.constants.earth.ω;
 
   return orb.v.mm( orb.v.r( ω * Δt, 3), x );
+};
 
+orb.transformations.fixedToInertial = function(x, Δt, ω, axis) {
+
+  return orb.transformations.inertialToFixed( x, Δt, -ω, axis );
 };
--- test/transformations/inertialToFixed.js
+++ test/transformations/inertialToFixed.js
@@ -29,3 +29,31 @@ describe('orb.transformations.inertialToFixed', function() {
   });
 
 });
+
+describe('orb.transformations.fixedToInertial', function() {
+
+  var x1 = [
+    1, 0, 0.2
+  ];
+
+  var ω = 2*Math.PI;
+
+  it('should be rotated -pi/2 after 1/4 second', function() {
+
+    var fixed = orb.transformations.fixedToInertial(x1, 0.25, ω, 3);
+    fixed[0].should.be.approximately( 0,   1e-15);
+    fixed[1].should.be.approximately( 1,   1e-15);
+    fixed[2].should.be.approximately( 0.2, 1e-15);
+
+  });
+
+  it('should be in same position after 1 second', function() {
+
+    var fixed = orb.transformations.fixedToInertial(x1, 1, ω, 3);
+    fixed[0].should.be.approximately( 1,   1e-15);
+    fixed[1].should.be.approximately( 0,   1e-15);
+    fixed[2].should.be.approximately( 0.2, 1e-15);
+
+  });
+
+});
