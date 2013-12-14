0092aba63ea552e605d03924074577efcb8801c5
Ben Elsen <ben.elsen@gmail.com>
2013-12-14 19:42:55 +0100

    [ADD] tests for fixedToTopocentric

---
+++ test/transformations/fixedToTopocentric.js
@@ -0,0 +1,45 @@
+var should = require('should');
+
+var orb = require('./../../');
+
+describe('orb.transformations.fixedToTopocentric', function() {
+
+  it('should correctly perform B rotation', function() {
+
+    var x1 = [
+      1, 0, Math.sqrt( 3 )
+    ];
+
+    var obs = [
+      0,
+      60 * Math.PI/180,
+      0
+    ];
+
+    var topo = orb.transformations.fixedToTopocentric(x1, obs, 1, 0);
+    topo[0].should.be.approximately(0, 1e-15);
+    topo[1].should.be.approximately(0, 1e-15);
+    topo[2].should.be.approximately(1, 1e-15);
+
+  });
+
+  it('should correctly perform L rotation', function() {
+
+    var x1 = [
+      1, 1, 0
+    ];
+
+    var obs = [
+      45 * Math.PI/180,
+      0,
+      0
+    ];
+
+    var topo = orb.transformations.fixedToTopocentric(x1, obs, 1, 0);
+    topo[0].should.be.approximately(0, 1e-15);
+    topo[1].should.be.approximately(0, 1e-15);
+    topo[2].should.be.approximately(Math.SQRT2 - 1, 1e-15);
+
+  });
+
+});
