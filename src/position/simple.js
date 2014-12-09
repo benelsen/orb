4ab303b445a94bc89e6fd21bbe8cd3e9c4ce8dbf
Ben Elsen <mail@benelsen.com>
2014-12-09 05:52:22 +0100

    Update simple.js

--- src/position/simple.js
+++ src/position/simple.js
@@ -1,16 +1,22 @@
-var constants = require('../constants').constants,
-    keplerEquation = require('./keplerEquation').keplerEquation,
-    orbitalPlaneToInertial = require('../transformations/orbitalPlaneToInertial').orbitalPlaneToInertial;
+var constants = require('../constants').constants;
+var keplerEquation = require('./keplerEquation').keplerEquation;
+var orbitalPlaneToInertial = require('../transformations/orbitalPlaneToInertial').orbitalPlaneToInertial;
 
-var simple = function(a, e, i, Ω, ω, t, t0, M0, m1, m2) {
+var keplerian = function keplerian (a, e, i, Ω, ω, t, t0, M0, m1, m2) {
 
-  if ( !M0 ) M0 = 0;
+  if ( !M0 ) {
+    M0 = 0;
+  }
 
   var GM = constants.earth.GM;
 
-  if ( m1 ) GM = constants.common.G * m1;
+  if ( m1 ) {
+    GM = constants.common.G * m1;
+  }
 
-  if ( m2 ) GM = constants.common.G * (m1 + m2);
+  if ( m2 ) {
+    GM = constants.common.G * (m1 + m2);
+  }
 
   var p = a * (1 - e*e);
 
@@ -18,13 +24,13 @@ var simple = function(a, e, i, Ω, ω, t, t0, M0, m1, m2) {
   var n = Math.sqrt( GM / Math.pow(a,3) );
 
   // Mean anomaly at t
-  var M = ( M0 + (n * ( t - t0 )) );
+  var M = M0 + n * ( t - t0 );
 
   // Eccentric anomaly
   var E = keplerEquation(e, M);
 
   // True anomaly
-  var ν = 2*Math.atan( Math.sqrt((1+e)/(1-e)) * Math.tan(E/2) );
+  var ν = 2 * Math.atan( Math.sqrt((1+e)/(1-e)) * Math.tan(E/2) );
   // var ν = Math.atan2( Math.sqrt(1-e*e) * Math.sin(E), Math.cos(E) - e );
 
   // radius
@@ -43,10 +49,11 @@ var simple = function(a, e, i, Ω, ω, t, t0, M0, m1, m2) {
      0
   ];
 
-  var x = orbitalPlaneToInertial(x_o, Ω, ω, i),
-      xdot = orbitalPlaneToInertial(xdot_o, Ω, ω, i);
+  var x = orbitalPlaneToInertial(x_o, Ω, ω, i);
+  var xdot = orbitalPlaneToInertial(xdot_o, Ω, ω, i);
 
   return [x, xdot];
 };
 
-exports.simple = simple;
+exports.keplerian = keplerian;
+exports.simple = exports.keplerian;
