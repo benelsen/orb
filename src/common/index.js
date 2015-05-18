dd2577c2210700bfdbbca167f955d770fad1e533
Ben Elsen <mail@benelsen.com>
2015-05-18 20:00:37 +0200

    Use pkg.json to set version

--- src/common/index.js
+++ src/common/index.js
@@ -1,8 +1,2 @@
 
-import {deg2rad, rad2deg} from './angular';
-
-const common = {
-  deg2rad, rad2deg
-};
-
-export default common;
+export * from './angular';
--- src/orb.js
+++ src/orb.js
@@ -1,14 +1,16 @@
 
 import './polyfill';
 
-import common from './common';
+import pkg from '../package.json';
+
+import * as common from './common';
 import constants from './constants';
 import time from './time';
 import vector from './vector';
 import transformations from './transformations';
 import position from './position';
 
-const version = '0.2.2';
+const version = pkg.version;
 
 const orb = {
   version, common, constants, time, vector, v: vector, transformations, position
