ae46f8e6da9624ead2800caa617362d25e1ef652
Ben Elsen <mail@benelsen.com>
2015-05-18 21:15:13 +0200

    Improve documentation in time conversion functions

--- src/time/conversions.js
+++ src/time/conversions.js
@@ -8,52 +8,93 @@ import * as constants from '../constants/time';
 
 /*eslint-disable new-cap */
 
-// JD -> MJD [days]
+/**
+ * Converts JD to MJD
+ * @param   {number} jd  Julian Date
+ * @returns {number} mjd Modified Julian Date
+ */
 export function JDtoMJD (jd) {
   return jd + constants.MJDJD;
 }
 
-// MJD -> JD [days]
+/**
+ * Converts MJD to JD
+ * @param   {number} mjd Modified Julian Date
+ * @returns {number} jd  Julian Date
+ */
 export function MJDtoJD (mjd) {
   return mjd - constants.MJDJD;
 }
 
-// TAI -> TT
+/**
+ * Converts TAI to TT
+ * @param   {number} tai TAI (seconds)
+ * @returns {number} tt  TT (seconds)
+ */
 export function TAItoTT (tai) {
   return tai + constants.TTTAI;
 }
 
-// TT -> TAI
+/**
+ * Converts TT to TAI
+ * @param   {number} tt  TT (seconds)
+ * @returns {number} tai TAI (seconds)
+ */
 export function TTtoTAI (tt) {
   return tt - constants.TTTAI;
 }
 
+/**
+ * Converts TAI to UTC
+ * @param   {number} tai TAI (seconds)
+ * @returns {number} utc UTC (seconds)
+ */
 // TAI -> UTC
 export function TAItoUTC (tai) {
   return tai - leapSeconds(new Date( tai * 1e3 ));
 }
 
-// UTC -> TAI
+/**
+ * Converts UTC to TAI
+ * @param   {number} utc UTC (seconds)
+ * @returns {number} tai TAI (seconds)
+ */
 export function UTCtoTAI (utc) {
   return utc + leapSeconds(new Date( utc * 1e3 ));
 }
 
-// TAI -> GPS
+/**
+ * Converts TAI to GPS
+ * @param   {number} tai TAI (seconds)
+ * @returns {number} gps GPS (seconds)
+ */
 export function TAItoGPS (tai) {
   return tai - constants.TAIGPS;
 }
 
-// GPS -> TAI
+/**
+ * Converts GPS to TAI
+ * @param   {number} gps GPS (seconds)
+ * @returns {number} tai TAI (seconds)
+ */
 export function GPStoTAI (gps) {
   return gps + constants.TAIGPS;
 }
 
-// UTC -> GPS
+/**
+ * Converts UTC to GPS
+ * @param   {number} utc UTC (seconds)
+ * @returns {number} gps GPS (seconds)
+ */
 export function UTCtoGPS (utc) {
   return TAItoGPS( UTCtoTAI(utc) );
 }
 
-// GPS -> UTC
+/**
+ * Converts GPS to UTC
+ * @param   {number} gps GPS (seconds)
+ * @returns {number} utc UTC (seconds)
+ */
 export function GPStoUTC (gps) {
   return TAItoUTC( GPStoTAI(gps) );
 }
