70f963c87078bedb6cb7ee98933e9e554f65892d
Ben Elsen <mail@benelsen.com>
2015-05-25 21:46:24 +0200

    Fix operator precedence bug.

--- src/functions/era.js
+++ src/functions/era.js
@@ -5,5 +5,5 @@
  * @return {number}      ERA (radians)
  */
 export function era(days) {
-  return 2 * Math.PI * ( 0.7790572732640 + 1.00273781191135448 * days ) % (2 * Math.PI);
+  return 2 * Math.PI * ( ( 0.7790572732640 + 1.00273781191135448 * days ) % 1 );
 }
